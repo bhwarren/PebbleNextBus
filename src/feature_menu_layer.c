4285f585ca744ca34e259e95b468f7b4411dabe6
bhwarren <linuxcoyote46@gmail.com>
2014-10-25 17:56:04 -0400

    Initial menu and bus menu implemented
    need to fix the overlapping issue

--- src/feature_menu_layer.c
+++ src/feature_menu_layer.c
@@ -1,20 +1,20 @@
 #include "pebble.h"
 
 #define NUM_MENU_SECTIONS 2
-#define NUM_MENU_ICONS 3
-#define NUM_FIRST_MENU_ITEMS 3
+#define NUM_FIRST_MENU_ITEMS 4
 #define NUM_SECOND_MENU_ITEMS 1
+	
+#define NUM_NEARBY_BUSES 5
+#define NUM_NEARBY_MENU_SECTIONS 1
 
 static Window *window;
 
 // This is a menu layer
 // You have more control than with a simple menu layer
+Layer *window_layer;
 static MenuLayer *menu_layer;
+static MenuLayer *nearby_menu_layer;
 
-// Menu items can optionally have an icon drawn with them
-static GBitmap *menu_icons[NUM_MENU_ICONS];
-
-static int current_icon = 0;
 
 // You can draw arbitrary things in a menu item such as a background
 static GBitmap *menu_background;
@@ -23,6 +23,9 @@ static GBitmap *menu_background;
 // With this, you can dynamically add and remove sections
 static uint16_t menu_get_num_sections_callback(MenuLayer *menu_layer, void *data) {
   return NUM_MENU_SECTIONS;
+}
+static uint16_t nearby_menu_get_num_sections_callback(MenuLayer *menu_layer, void *data) {
+  return NUM_NEARBY_MENU_SECTIONS;
 }
 
 // Each section has a number of items;  we use a callback to specify this
@@ -40,10 +43,26 @@ static uint16_t menu_get_num_rows_callback(MenuLayer *menu_layer, uint16_t secti
   }
 }
 
+static uint16_t nearby_menu_get_num_rows_callback(MenuLayer *menu_layer, uint16_t section_index, void *data) {
+	switch(section_index){
+		case 0:
+			return NUM_NEARBY_BUSES;
+		//only allow one "section"
+		default:
+			return 0;
+	}
+}
+
+
+
 // A callback is used to specify the height of the section header
 static int16_t menu_get_header_height_callback(MenuLayer *menu_layer, uint16_t section_index, void *data) {
   // This is a define provided in pebble.h that you may use for the default height
   return MENU_CELL_BASIC_HEADER_HEIGHT;
+}
+
+static int16_t nearby_menu_get_header_height_callback(MenuLayer *menu_layer, uint16_t section_index, void *data) {
+	return MENU_CELL_BASIC_HEADER_HEIGHT;
 }
 
 // Here we draw what each header is
@@ -52,12 +71,20 @@ static void menu_draw_header_callback(GContext* ctx, const Layer *cell_layer, ui
   switch (section_index) {
     case 0:
       // Draw title text in the section header
-      menu_cell_basic_header_draw(ctx, cell_layer, "Some example items");
+      menu_cell_basic_header_draw(ctx, cell_layer, "PebbleNextbus");
       break;
+  }
+}
 
-    case 1:
-      menu_cell_basic_header_draw(ctx, cell_layer, "One more");
+static void nearby_menu_draw_header_callback(GContext* ctx, const Layer *cell_layer, uint16_t section_index, void *data) {
+  // Determine which section we're working with
+  switch (section_index) {
+    case 0:
+      // Draw title text in the section header
+      menu_cell_basic_header_draw(ctx, cell_layer, "Nearby Buses");
       break;
+		default:
+			break;
   }
 }
 
@@ -69,57 +96,93 @@ static void menu_draw_row_callback(GContext* ctx, const Layer *cell_layer, MenuI
       // Use the row to specify which item we'll draw
       switch (cell_index->row) {
         case 0:
-          // This is a basic menu item with a title and subtitle
-          menu_cell_basic_draw(ctx, cell_layer, "Basic Item", "With a subtitle", NULL);
+          // This is a basic menu item with a title and subtitle - ""
+          menu_cell_basic_draw(ctx, cell_layer, "Nearby Buses", "", NULL);
           break;
+				case 1:
+		  		menu_cell_basic_draw(ctx, cell_layer, "Select Stop", "From a predefined list", NULL);
+		  		break;
+				case 2:
+		  		menu_cell_basic_draw(ctx, cell_layer, "Favorite Stops", "", NULL);
+		  		break;
+				case 3:
+		  		menu_cell_basic_draw(ctx, cell_layer, "Information", "", NULL);
+		  		break;
+			}
+			break;
+    default:
+		  break;
+  }
+}
 
-        case 1:
-          // This is a basic menu icon with a cycling icon
-          menu_cell_basic_draw(ctx, cell_layer, "Icon Item", "Select to cycle", menu_icons[current_icon]);
+static void nearby_menu_draw_row_callback(GContext* ctx, const Layer *cell_layer, MenuIndex *cell_index, void *data) {
+  // Determine which section we're going to draw in
+  switch (cell_index->section) {
+    case 0:
+      // Use the row to specify which item we'll draw
+      switch (cell_index->row) {
+        case 0:
+          // This is a basic menu item with a title and subtitle - ""
+          menu_cell_basic_draw(ctx, cell_layer, "A Longview 33min", "Towards Family Practice", NULL);
           break;
-
-        case 2:
-          // Here we use the graphics context to draw something different
-          // In this case, we show a strip of a watchface's background
-          graphics_draw_bitmap_in_rect(ctx, menu_background,
-              (GRect){ .origin = GPointZero, .size = layer_get_frame((Layer*) cell_layer).size });
-          break;
+				case 1:
+					menu_cell_basic_draw(ctx, cell_layer, "N Longview 10min", "Towards Town Hall", NULL);
+		  		break;
+				case 2:
+		  		menu_cell_basic_draw(ctx, cell_layer, "JFX Sitterson 2min", "Towards Jones Ferry Rd", NULL);
+		  		break;
       }
+		
       break;
 
     case 1:
       switch (cell_index->row) {
-        case 0:
-          // There is title draw for something more simple than a basic menu item
-          menu_cell_title_draw(ctx, cell_layer, "Final Item");
-          break;
+				default:
+		  		break;
       }
   }
+}
+
+	
+void load_nearby_menu(){
+	menu_layer_set_click_config_onto_window(nearby_menu_layer, window);
+	layer_add_child(window_layer, menu_layer_get_layer(nearby_menu_layer));
 }
 
 // Here we capture when a user selects a menu item
 void menu_select_callback(MenuLayer *menu_layer, MenuIndex *cell_index, void *data) {
   // Use the row to specify which item will receive the select action
   switch (cell_index->row) {
-    // This is the menu item with the cycling icon
-    case 1:
-      // Cycle the icon
-      current_icon = (current_icon + 1) % NUM_MENU_ICONS;
-      // After changing the icon, mark the layer to have it updated
-      layer_mark_dirty(menu_layer_get_layer(menu_layer));
-      break;
-  }
+		case 0:
+			//make a new menu with list of buses close by
+			load_nearby_menu();
+			break;
+		case 1:
+			//make a new menu of stops available nearby
+			break;
+		case 2:
+			//make a new menu to choose favorite stops already saved
+			break;
+		case 3:
+			//show a text info screen
+			break;
+	}
+}
 
+void load_bus_info(int row){
+	
+	
+}
+
+void nearby_menu_select_callback(MenuLayer *menu_layer, MenuIndex *cell_index, void *data) {
+  // Use the row to specify which item will receive the select action
+	load_bus_info(cell_index->row);
 }
 
 // This initializes the menu upon window load
 void window_load(Window *window) {
   // Here we load the bitmap assets
   // resource_init_current_app must be called before all asset loading
-  int num_menu_icons = 0;
-  menu_icons[num_menu_icons++] = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_MENU_ICON_BIG_WATCH);
-  menu_icons[num_menu_icons++] = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_MENU_ICON_SECTOR_WATCH);
-  menu_icons[num_menu_icons++] = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_MENU_ICON_BINARY_WATCH);
 
   // And also load the background
   menu_background = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_BACKGROUND_BRAINS);
@@ -127,11 +190,12 @@ void window_load(Window *window) {
   // Now we prepare to initialize the menu layer
   // We need the bounds to specify the menu layer's viewport size
   // In this case, it'll be the same as the window's
-  Layer *window_layer = window_get_root_layer(window);
+  window_layer = window_get_root_layer(window);
   GRect bounds = layer_get_frame(window_layer);
 
   // Create the menu layer
   menu_layer = menu_layer_create(bounds);
+  nearby_menu_layer = menu_layer_create(bounds);
 
   // Set all the callbacks for the menu layer
   menu_layer_set_callbacks(menu_layer, NULL, (MenuLayerCallbacks){
@@ -142,9 +206,18 @@ void window_load(Window *window) {
     .draw_row = menu_draw_row_callback,
     .select_click = menu_select_callback,
   });
+	menu_layer_set_callbacks(nearby_menu_layer, NULL, (MenuLayerCallbacks){
+		.get_num_sections = nearby_menu_get_num_sections_callback,
+    .get_num_rows = nearby_menu_get_num_rows_callback,
+    .get_header_height = nearby_menu_get_header_height_callback,
+    .draw_header = nearby_menu_draw_header_callback,
+    .draw_row = nearby_menu_draw_row_callback,
+    .select_click = nearby_menu_select_callback,
+	});
 
   // Bind the menu layer's click config provider to the window for interactivity
   menu_layer_set_click_config_onto_window(menu_layer, window);
+	
 
   // Add it to the window for display
   layer_add_child(window_layer, menu_layer_get_layer(menu_layer));
@@ -153,11 +226,6 @@ void window_load(Window *window) {
 void window_unload(Window *window) {
   // Destroy the menu layer
   menu_layer_destroy(menu_layer);
-
-  // Cleanup the menu icons
-  for (int i = 0; i < NUM_MENU_ICONS; i++) {
-    gbitmap_destroy(menu_icons[i]);
-  }
 
   // And cleanup the background
   gbitmap_destroy(menu_background);
