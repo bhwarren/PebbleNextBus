88c526fa503be9daa747c91f935c5ff6f0e05467
Bo Warren <bowarren46@gmail.com>
2015-01-22 21:35:27 -0500

    Put todo list in todo.c
    cloudpebble doesn't show the Readme.md file, so this way is easier w/out committing twice everytime

---
+++ src/Todo.c
@@ -0,0 +1,14 @@
+#include <pebble.h>
+/* 
+
+------ Todo ------
+
+- clean up code
+- implement favorites
+- populate information tab
+- resize/reposition bus page to make things more clear
+- re-do the loading page
+- add support for other agencies besides Chapel Hill
+
+
+*/
